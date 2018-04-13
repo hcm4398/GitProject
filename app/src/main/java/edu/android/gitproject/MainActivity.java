351e3deaade70ae8a67d248ad1e197b9364b2963
stu <hcm4398@gmail.com>
2018-04-13 14:19:43 +0900

    button을 지역 변수로

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -7,14 +7,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-   TextView textView = findViewById(R.id.textview);
+    TextView textView = findViewById(R.id.textview);
     Button  button = findViewById(R.id.button);
     }
 }
