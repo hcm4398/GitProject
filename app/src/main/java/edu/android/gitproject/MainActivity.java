c7e6d8281e798b18ce0bcce077a5e3f8fa1e1975
stu <hcm4398@gmail.com>
2018-04-13 16:49:59 +0900

    .

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
 
     TextView textView = findViewById(R.id.textview);
     Button  button = findViewById(R.id.button);
